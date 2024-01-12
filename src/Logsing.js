4003f9d18df71261aaee7883ab55b1080026f771
Aman Kumar <96297924+amansinha0401@users.noreply.github.com>
2024-01-12 10:58:11 +0530

    Logsing.js

--- src/Logsing.js
+++ src/Logsing.js
@@ -10,7 +10,7 @@ const Logsing = () => {
 
   const logUser = async () => {
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://raildk.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
