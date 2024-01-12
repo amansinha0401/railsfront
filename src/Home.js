bc3c23889e728b651e4b9df2856e423f2da01271
Aman Kumar <96297924+amansinha0401@users.noreply.github.com>
2024-01-12 11:18:37 +0530

    Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <div className='okay'><h1>Welcome to the indian railways <br/>
+      <div className='okay'><h1>Wellcome to the indian railways <br/>
       The centralised attendace system </h1></div>
     </div>
   )
